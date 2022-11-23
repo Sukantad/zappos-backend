8e0dcae76cf09dd86358bbba0513a95f6014e67e
Gulshan7777 <sharmagulshan252@gmail.com>
2022-11-23 12:08:16 +0530

    Added files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors= require("cors")
-const conn = require("./database/db")
+const conn = require("./database/db");
 
 const app=express();
 app.use(express.json())
