bf62be12244cff2eeed6617ea0c7cf68828ff0c1
Gulshan7777 <sharmagulshan252@gmail.com>
2022-11-23 15:38:45 +0530

    added atlas

--- database/db.js
+++ database/db.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({
+   path:"./.env"
+})
 
+const API=process.env.MONGO_API;
 function conn(){
-   mongoose.connect("mongodb://localhost:27017/zappos")
+   mongoose.connect(API)
    .then(()=>{
     console.log("Connected to Database")
    })
