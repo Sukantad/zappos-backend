6f525d4b3c436bfd02838321c6e1a3c663725b66
Umesh Kumawat <68832856+Royanosh@users.noreply.github.com>
2022-11-24 18:02:45 +0530

    confirm password

--- middleware/detailsValidator.js
+++ middleware/detailsValidator.js
@@ -1,5 +1,20 @@
 const { check } = require("express-validator");
 
+const varifyPassword = (req, res, next) => {
+  if (!req.body.confirmPassword) {
+    return res.status(400).send({
+      status: "error",
+      data: "Please enter confirm password",
+    });
+  } else if (req.body.password === req.body.confirmPassword) {
+    next();
+  } else {
+    return res.status(400).send({
+      status: "error",
+      data: "Confirm password must be same as password",
+    });
+  }
+};
 //signup details validating
 exports.signupDetailsValidator = [
   check("name", "Name atleast should be 3 characters").isLength({ min: 3 }),
@@ -7,6 +22,7 @@ exports.signupDetailsValidator = [
   check("password", "Password at least should be 6 characters").isLength({
     min: 6,
   }),
+  varifyPassword,
 ];
 
 //login details validating
