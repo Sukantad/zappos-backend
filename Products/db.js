4f43c0411358262107fc3582357f81547d5e804d
ranjanabhi00 <81633099+ranjanabhi00@users.noreply.github.com>
2022-11-24 21:10:14 +0530

    Product Merging step 1

--- Products/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const url="mongodb+srv://Abhishek:Abhishek1234@cluster0.lmieazm.mongodb.net/?retryWrites=true&w=majority";
-const url2="mongodb://127.0.0.1:27017"
-
-const connectDatabase = async () => {
-    await mongoose.connect(url);
-    console.log("Db connected")
-}
-module.exports=connectDatabase;
