16fbbe49997d9458f0131a30db56d9f2d9ae4b95
sukantad <ghoshsu4@gmail.com>
2022-11-24 23:58:12 +0530

    multi sort & pagination done

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -3,41 +3,44 @@ const { product_model } = require("../models/product.model");
 exports.getProducts = async (req, res) => {
   try {
     let queries = req.query;
-    // price sorting
-    if (queries.sort === "price" && queries.order != "") {
+
+    if (
+      Number(queries.page) &&
+      Number(queries.limit) &&
+      queries.q != null &&
+      queries.sort != null &&
+      queries.order != null
+    ) {
+   
       let a = 1;
       if (queries.order == "desc") {
         a = -1;
       }
-      const product = await product_model.find().sort({ price: a });
+      const product = await product_model
+        .find({ $text: { $search: queries.q } })
+        .sort({ price: a })
+        .skip(
+          Number(queries.limit) * Number(queries.page) - Number(queries.limit)
+        )
+        .limit(queries.limit);
       return res.send(product);
     }
-    // rating sorting
-    if (queries.sort === "ratings" && queries.order != "") {
-      let a = 1;
-      if (queries.order === "desc") {
-        a = -1;
-      }
-      const product = await product_model.find().sort({ ratings: a });
-      return res.send(product);
+    if (queries.page && queries.limit && queries.q != null) {
+      const product = await product_model
+        .find({ $text: { $search: queries.q } })
+        .skip(
+          Number(queries.limit) * Number(queries.page) - Number(queries.limit)
+        )
+        .limit(queries.limit);
+      return res.send(product); 
     }
-    // search
-    // if (queries.q != "") {
-    //   var qu = queries.q;
-    //   console.log(qu);
-
-    //   const product = await product_model.find({
-    //     $text: { $search: queries.q },
-    //   });
-    //   return res.send(product);
-    // }
-    // pagination
     if (
       queries.page &&
       queries.limit &&
-      queries.sort == "price" &&
+      queries.sort != null &&
       queries.order != null
     ) {
+      console.log("here");
       let a = 1;
       if (queries.order == "desc") {
         a = -1;
@@ -51,20 +54,30 @@ exports.getProducts = async (req, res) => {
         .limit(queries.limit);
       return res.send(product);
     }
-    // only pagination
-    if (queries.page && queries.limit) {
+    if (queries.q != null && queries.sort != null && queries.order != null) {
+      let a = 1;
+      if (queries.order == "desc") {
+        a = -1;
+      }
       const product = await product_model
-        .find()
-        .skip(
-          Number(queries.limit) * Number(queries.page) - Number(queries.limit)
-        )
-        .limit(queries.limit);
+        .find({ $text: { $search: queries.q } })
+        .sort({ price: a });
       return res.send(product);
     }
-    else{
-      const product = await product_model.find();
-      return res.send(product)
+    if (queries.q) {
+      const Movie = await product_model.find({ $text: { $search: queries.q } });
+      return res.send(Movie);
     }
+    if (queries.sort === "price" && queries.order != "") {
+      let a = 1;
+      if (queries.order == "desc") {
+        a = -1;
+      }
+      const product = await product_model.find().sort({ price: a });
+      return res.send(product);
+    }
+    const product = await product_model.find();
+    return res.send(product);
   } catch (error) {
     return res.status(500).send(error.message);
   }
