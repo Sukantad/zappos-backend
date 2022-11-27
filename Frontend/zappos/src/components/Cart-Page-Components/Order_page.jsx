0946d144925a40f6bbe658162745ed77d0020ed6
Umesh Kumawat <68832856+Royanosh@users.noreply.github.com>
2022-11-27 20:56:46 +0530

    Checkout

--- Frontend/zappos/src/components/Cart-Page-Components/Order_page.jsx
+++ Frontend/zappos/src/components/Cart-Page-Components/Order_page.jsx
@@ -1,36 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import "./shipping.css"
+import React, { useEffect, useState } from "react";
+import "./shipping.css";
 
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 const Order_page = () => {
-
   const cart = useSelector((state) => state.cart);
-  const [ totalAmount, setTotalAmount ] = useState(0);
-  
-  
-  useEffect(()=>{
-    const getAmount = ()=>{
-      let amt = cart.reduce((acc, elem)=>{
-          return acc + elem.price*elem.count
-      },0)
-      setTotalAmount(Math.floor(amt));
-    }
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    const getAmount = () => {
+      let amt = cart?.reduce((acc, elem) => {
+        return acc + elem.productId.price * elem.quantity;
+      }, 0);
+      setTotalAmount(amt ? Math.floor(amt) : 0);
+    };
     getAmount();
-  },[cart])
-
-
-  
+  }, [cart]);
 
   return (
     <>
-    <div className='container'>
-        <div id="right" className='right'>
-        <div>
-        </div>
+      <div className="container">
+        <div id="right" className="right">
+          <div></div>
           <div className="div2">
-            
-           <button  className="payment-btn place">PLACE YOUR ORDER</button>
-            <h3 className="order-summary">Order Summary ({cart.length} Items)</h3>
+            <button className="payment-btn place">PLACE YOUR ORDER</button>
+            <h3 className="order-summary">
+              Order Summary ({cart?.length} Items)
+            </h3>
             <div className="divide subtotal">
               <h3>Subtotal:</h3>
               <h3 className="subtotalPrice">${totalAmount}</h3>
@@ -61,9 +56,9 @@ const Order_page = () => {
             </button>
           </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Order_page
+export default Order_page;
